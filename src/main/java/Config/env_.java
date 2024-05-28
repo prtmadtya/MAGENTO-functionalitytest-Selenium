55358821ad74d38d3de555d677ac9fbd08ab2579
Aditya Pratama <aditya1995.jr@gmail.com>
2024-05-28 20:45:04 +0700

    Add Init

---
+++ src/main/java/Config/env_.java
@@ -0,0 +1,9 @@
+package Config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env_ {
+    protected WebDriver driver;
+
+    protected String BaseUrl ("https://magento.softwaretestingboard.com/");
+}
---
+++ src/main/java/TestCase/login.java
@@ -0,0 +1,25 @@
+package TestCase;
+import Config.env_;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class login extends env_ {
+    @Test
+    public void main() {
+        //set driver location path
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //set url
+        driver.get(baseurl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'] [@id='login-button']"))
+        );
+    }
+}
